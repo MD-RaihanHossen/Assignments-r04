582d8f133cc62fac50b20c40b0339130d41b5071
Raihanhossen <raihanhossen.eee@gmail.com>
2022-10-01 23:21:56 +0600

    data input

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
-function radianToDegree(radian){
-    const oneRadian = 57.2958;
-    const convertToDegree = radian * oneRadian;
-    const result = parseFloat(convertToDegree.toFixed(2))
-    return result
+//1st problame 
+// function radianToDegree(radian){
+//     const oneRadian = 57.2958;
+//     const convertToDegree = radian * oneRadian;
+//     const result = parseFloat(convertToDegree.toFixed(2))
+//     return result
+// }
+
+// const radian = 15; 
+// const result = radianToDegree(radian)
+// console.log(result)
+
+//2nd problame
+function  isJavaScriptFile(fileName){
+    // const trueFileName = fileName.includes('.js');
+    if(fileName.includes('.js') !== false){
+        return true;
+    }
+    else{
+        return false;
+    }
 }
 
-const radian = 15; 
-const result = radianToDegree(radian)
+const fileName = 'index.js';
+const result = isJavaScriptFile(fileName);
 console.log(result)
+
