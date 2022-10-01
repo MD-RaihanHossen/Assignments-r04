ce82abbbea9c073b8c6e0cd560ea499e15437943
Raihanhossen <raihanhossen.eee@gmail.com>
2022-10-01 23:48:54 +0600

    3nd problam solve

--- index.js
+++ index.js
@@ -10,18 +10,37 @@
 // const result = radianToDegree(radian)
 // console.log(result)
 
+
 //2nd problame
-function  isJavaScriptFile(fileName){
-    // const trueFileName = fileName.includes('.js');
-    if(fileName.includes('.js') !== false){
-        return true;
-    }
-    else{
-        return false;
-    }
-}
+// function  isJavaScriptFile(fileName){
 
-const fileName = 'index.js';
-const result = isJavaScriptFile(fileName);
-console.log(result)
+//     if(fileName.includes('.js') !== false){
+//         return true;
+//     }
+//     else{
+//         return false;
+//     }
+// }
 
+// const fileName = 'index.js';
+// const result = isJavaScriptFile(fileName);
+// console.log(result)
+
+
+// function oilPrice(diesel, petrol, octane){
+//     const dieselPrice = 114;
+//     const petrolPrice = 130;
+//     const octanePrice = 135;
+//     const totalDieselPrice = dieselPrice * diesel;
+//     const totalPetrolPrice = petrolPrice * petrol;
+//     const totalOctanePrice = octanePrice * octane;
+//     const totalPrice = totalDieselPrice + totalPetrolPrice + totalOctanePrice;
+//     return totalPrice;
+    
+// }
+
+// const dieselLitter = 0;
+// const petrolLitter = 1;
+// const octaneLitter = 1;
+// const result  = oilPrice(dieselLitter, petrolLitter, octaneLitter);
+// console.log(result);
