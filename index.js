6581b66d1d0790e9ff6f5498397c54851edd06e7
Raihanhossen <raihanhossen.eee@gmail.com>
2022-10-02 23:17:00 +0600

    complite

--- index.js
+++ index.js
@@ -1,46 +1,102 @@
-//1st problame 
-// function radianToDegree(radian){
-//     const oneRadian = 57.2958;
-//     const convertToDegree = radian * oneRadian;
-//     const result = parseFloat(convertToDegree.toFixed(2))
-//     return result
-// }
-
-// const radian = 15; 
-// const result = radianToDegree(radian)
-// console.log(result)
 
 
-//2nd problame
-// function  isJavaScriptFile(fileName){
-
-//     if(fileName.includes('.js') !== false){
-//         return true;
-//     }
-//     else{
-//         return false;
-//     }
-// }
-
-// const fileName = 'index.js';
-// const result = isJavaScriptFile(fileName);
-// console.log(result)
+function radianToDegree(radian) {
+    if(typeof radian !== "number"){
+        const error = "please valid input"
+        return error;
+    }
+   else{
+    const oneRadian = 57.2958;
+    const convertToDegree = radian * oneRadian;
+    const result = parseFloat(convertToDegree.toFixed(2))
+    return result
+   }
+}
 
 
-// function oilPrice(diesel, petrol, octane){
-//     const dieselPrice = 114;
-//     const petrolPrice = 130;
-//     const octanePrice = 135;
-//     const totalDieselPrice = dieselPrice * diesel;
-//     const totalPetrolPrice = petrolPrice * petrol;
-//     const totalOctanePrice = octanePrice * octane;
-//     const totalPrice = totalDieselPrice + totalPetrolPrice + totalOctanePrice;
-//     return totalPrice;
+
+
+
+
+function  isJavaScriptFile(fileName) {
+
+    if(typeof fileName !== 'string'){
+        const error = "please valid input"
+        return error;
+    }
+                
+        else if (fileName.includes('.js') !== false) {
+            return true;
+        }
+        else {
+            return false;
+        }
+   
+}
+
+
+
+
+
+
+function oilPrice(diesel, petrol, octane) {
+   if(typeof diesel=== "number" && typeof petrol === "number" && typeof octane === "number"){
+    const dieselPrice = 114;
+    const petrolPrice = 130;
+    const octanePrice = 135;
+    const totalDieselPrice = dieselPrice * diesel;
+    const totalPetrolPrice = petrolPrice * petrol;
+    const totalOctanePrice = octanePrice * octane;
+    const totalPrice = totalDieselPrice + totalPetrolPrice + totalOctanePrice;
+    return totalPrice;
+   }
+   else{
+        const error = "please valid input"
+        return error;
+       }
+}
+
+
+
+function publicBusFare(students){
+    const publicCost = 250;
+   if(typeof students === 'number'){
+    const busRemaining = students % 50;
+    const microBusRemaining = busRemaining % 11;
+    const publicBustCountity = microBusRemaining * publicCost;
+    return publicBustCountity;
+   }
+   else{
+    const error = "please valid input"
+    return error;
+   }
+
     
-// }
+}
 
-// const dieselLitter = 0;
-// const petrolLitter = 1;
-// const octaneLitter = 1;
-// const result  = oilPrice(dieselLitter, petrolLitter, octaneLitter);
-// console.log(result);
+
+
+
+function isBestFriend(friends1, friends2){
+    const raihan = friends1.name;
+    const riya = friends2.friend;
+    
+    const raihan1 = friends1.friend;
+    const riya1 = friends2.name;
+
+    if(typeof raihan !== 'string' || typeof raihan1 !== 'string' || typeof riya !== 'string' || typeof riya1 !== 'string'){
+        const error = "please valid input"
+        return error;
+    }
+    
+    else if(raihan === riya && raihan1 === riya1){
+      return true; 
+    }
+    
+    else{
+      return false;
+    }
+  }
+  
+   
+    
